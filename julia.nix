3fe92c5a39a8319b91d0af9f4305e03fc1593166
Antoine Marteau <43928194+Antoinemarteau@users.noreply.github.com>
2025-07-03 11:46:51 +1000

    julia 1.10.10 fix hash

--- julia.nix
+++ julia.nix
@@ -5,7 +5,7 @@ let
     "1.11.5" = "sha256-cj6HjGQiIMwCUaDhN1jAWaOJytx/ATdv6vHqc4j+j5w=";
     "1.11.1" = "sha256-zKjRPcRQfk9ioSkyIpMxPuV08wDU3559swt7QcX4qPM=";
     "1.11.0" = "sha256-vPgVVT/aLteRBSTIyqGJyOgZGkCnmd2LX77Q2d1riCw=";
-    "1.10.10" = "sha256-Wi0sUiRZS2g8l+cwTLckB/vPC+SgGHeJy6Gi9z8Mvwk=";
+    "1.10.10" = "sha256-anigOnHHq3kuhnPcXO25GOA38IHOtYtQlx37fGTFv4E=";
     "1.10.9" = "sha256-Wi0sUiRZS2g8l+cwTLckB/vPC+SgGHeJy6Gi9z8Mvwk=";
     "1.10.4" = "sha256-B59hdXw7W0DSreBSs8xIFvUPfvbfZogldyVis3Rq3/E=";
     "1.9.4" = "sha256-B9IMTCUYgz4iZcoKzuFbNVRjNhqk79q4WNrYJs+UMlw="; 
